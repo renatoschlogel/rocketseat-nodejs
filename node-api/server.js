6c6b001ace7ed7d3597eced0a65382097c601220
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-02 01:26:50 -0300

    Criando a primeira rota

--- node-api/server.js
+++ node-api/server.js
@@ -1,5 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("Hello Renato!");
+});
 
 app.listen(3001);
