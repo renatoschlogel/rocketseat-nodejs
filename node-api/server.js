a8ee338b0919992025b2e2eb79de7862c86a62fe
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-02 01:34:08 -0300

    instalando e criando dependencia com nomemon

--- node-api/server.js
+++ node-api/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res)=>{
-    res.send("Hello Renato!");
+    res.send("Hello Renato opa!");
 });
 
 app.listen(3001);
