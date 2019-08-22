428a6ebc617327b99b076a2085bf2b47a5a9593e
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-22 01:01:10 -0300

    conclusao do CRUD

--- node-api/src/routes.js
+++ node-api/src/routes.js
@@ -8,5 +8,7 @@ const productController = require('./controllers/ProductController');
 routes.get("/products", productController.index);
 routes.get("/products/:id", productController.show);
 routes.post("/products", productController.store);
+routes.put("/products/:id", productController.update);
+routes.delete("/products/:id", productController.destroy);
 
 module.exports = routes;
