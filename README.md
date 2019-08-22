ce15ee476c7c8b6fa8c5ed9f6691dabc57664386
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-21 22:34:00 -0300

    conexao com o Banco de dados mongo

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Curso NodeJS da Rocketseat
 
 https://station.rocketseat.com.br/courses/starter
+
+    * Para executar a API com nodemon       : npm run dev
+
+Utilizando o MongoDB de uma imagem Docker:
+    * docker container run --name mongodb -p 27017:27017 -d mongo
+
--- node-api/server.js
+++ node-api/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+// iniciando app
 const app = express();
+
+// iniciando DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
 
 app.get("/", (req, res)=>{
     res.send("Hello Renato opa!");
