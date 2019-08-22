73e2bb40f86a6d036958725947b0e95d0cef2492
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-22 00:48:57 -0300

    Consulta por id

--- node-api/src/routes.js
+++ node-api/src/routes.js
@@ -6,7 +6,7 @@ const productController = require('./controllers/ProductController');
 
 
 routes.get("/products", productController.index);
-
+routes.get("/products/:id", productController.show);
 routes.post("/products", productController.store);
 
 module.exports = routes;
