e7816deea9af8b9b8dad571129f773e38f8dfc39
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-22 00:44:13 -0300

    correção ...

--- node-api/src/controllers/ProductController.js
+++ node-api/src/controllers/ProductController.js
@@ -4,7 +4,7 @@ const Product = mongoose.model('Product');
 
 module.exports = {
     async index(req, res){
-        const products = await product.find();
+        const products = await Product.find();
 
         return res.json(products);
     },
